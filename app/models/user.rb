57b8c75f3105dcf12ea4789755833f2c0d8622ae
johnsonsirv <johnsonsirv@gmail.com>
2019-07-19 20:03:27 +0000

    update validations for mode

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-	validates :email, :username, :password presence: true
-	validates :username :password, length: {in 8..16}
+	validates :email, :username, :password, presence: true
+	validates :username, :password, length: {in: 8..16}
+	validates :username, :password, uniqueness: true
 end
