a701671d8e786b5ea6542b3fa696dcf5bd18fd1a
johnsonsirv <johnsonsirv@gmail.com>
2019-07-20 15:42:22 +0000

    added test to check a user creates a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
 	validates :title, length: { maximum: 255 }
+	validates :user_id, presence: true
+	
+	
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,17 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
  	context "validations" do
+		it "should not be valid without a valid user_id" do
+			post_1 = Post.new(title: 'my title', body: 'my body', user_id: nil)
+			
+			expect(post_1).to_not be_valid
+		end
 		it "should not be valid with title length > 255" do 
-			post_1 = Post.new(title: 'my title '* 300, body: '', user_id: nil)
+			post_1 = Post.new(title: 'my title '* 300, body: '', user_id: 1)
 			
 			expect(post_1).to_not be_valid
 		end
 		it "should be valid with title length within 255" do 
-			post_1 = Post.new(title: 'my title'*5, body: '', user_id: nil)
+			post_1 = Post.new(title: 'my title'*5, body: '', user_id: 1)
 			
 			expect(post_1).to be_valid
-		end
-		it "should be created by a valid user" do
 		end
 	end
 end
