585ffa51a9442b6193dd5bc1445bf66651dfc74f
johnsonsirv <johnsonsirv@gmail.com>
2019-07-20 11:26:55 +0000

    test for presence of valid user email, username, passowrd

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	validates :email, :username, :password, presence: true
-	validates :username, :password, length: {in: 8..16}
-	validates :username, :password, uniqueness: true
+	# validates :username, :password, length: {in: 8..16}
+	# validates :username, :password, uniqueness: true
 	
 	
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,1 +1,15 @@
 require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+	context "validations" do
+		it "is not valid without an email, username, password" do
+			u = User.new
+			
+			expect(u).to_not be_valid
+		end
+		it "is valid with an email, username, password" do
+			u = User.new(email: "johnsonsirv@gmail.com", username: "johnsonsirv", password: "1234")
+			expect(u).to be_valid
+		end
+	end
+end
