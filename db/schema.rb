c263c97763c604a70fe1a1d91c1616ce21a6e667
johnsonsirv <johnsonsirv@gmail.com>
2019-07-20 14:03:17 +0000

    add databse constraint on unique columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_19_124151) do
+ActiveRecord::Schema.define(version: 2019_07_20_135919) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_07_19_124151) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["username"], name: "index_users_on_username"
   end
 
 end
