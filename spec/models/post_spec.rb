c330dafe1401c9673e0ffc7ae9a90c9b4c5fedd9
johnsonsirv <johnsonsirv@gmail.com>
2019-07-20 15:09:54 +0000

    added test to check length of title

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ 	context "validations" do
+		it "should not be valid with title length > 255" do 
+			post_1 = Post.new(title: 'my title '* 300, body: '', user_id: nil)
+			
+			expect(post_1).to_not be_valid
+		end
+		it "should be valid with title length within 255" do 
+			post_1 = Post.new(title: 'my title'*5, body: '', user_id: nil)
+			
+			expect(post_1).to be_valid
+		end
+		it "should be created by a valid user" do
+		end
+	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
+RSpec.describe User, type: :model do
 	context "validations" do
 		it "is not valid without an email, username, password" do
 			u = User.new
